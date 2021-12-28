6597477247084dcc2bbd41d83cc7e6ec83eb32f7
Georgi Korchakov <georgi@nas-10-240-92-47.york.ac.uk>
2021-12-28 23:04:11 +0000

    [Add] Sensors file. This file contains all of the sensor functionalities.

---
+++ include/sensors.h
@@ -0,0 +1,71 @@
+#ifndef SENSORS_H
+#define SENSORS_H
+
+#include <Arduino.h>
+#include "header.h"
+
+// More information about the Teensy ADC Library
+// https://forum.pjrc.com/threads/25532-ADC-library-update-now-with-support-for-Teensy-3-1
+#include <ADC.h>
+
+// More information about the Accelerometer and the Gyroscope 
+// and the library that we are useing.
+// https://learn.adafruit.com/lsm6ds33-6-dof-imu=accelerometer-gyro
+// More information about Gyroscope calibration
+// https://learn.adafruit.com/adafruit-sensorlab-gyroscope-calibration
+#include <Adafruit_LSM6DS33.h>
+
+// Pin definitions for the Adafruit_LSM6DS33.h library.
+#define LSM_SCK SCL
+#define LSM_MOSI SDA
+
+// More information about the Encoders library
+// https://www.pjrc.com/teensy/td_libs_Encoder.html
+#define ENCODER_OPTIMIZE_INTERRUPTS
+#include <Encoder.h>
+
+ADC *adc = new ADC();
+Adafruit_LSM6DS33 lsm6ds33;
+Encoder left_encoder(LEFT_MOTOR_ENCODER_A, LEFT_MOTOR_ENCODER_B);
+Encoder right_encoder(RIGHT_MOTOR_ENCODER_A, RIGHT_MOTOR_ENCODER_B);
+
+// It should probably be less than that. 
+// TODO: Read the emitter datasheet
+#define SENSOR_EMITTER_ON_TIME 320 // Microseconds
+
+// Global Variables
+uint16_t front_left_sensor = 0;
+uint16_t front_right_sensor = 0;
+uint16_t side_left_sensor = 0;
+uint16_t side_right_sensor = 0;
+uint16_t diagonal_left_sensor = 0;
+uint16_t diagonal_right_sensor = 0;
+
+float acc_x;
+float acc_y;
+float acc_z;
+
+float gyro_x;
+float gyro_y;
+float gyro_z;
+
+ADC::Sync_result emitter_on;
+ADC::Sync_result emitter_off;
+
+// Functions that take use of the ADC
+void init_adc();
+void read_proximity_sensors();
+void read_proximity_sensors_front();
+void read_proximity_sensors_diagonal();
+void read_proximity_sensors_side();
+float read_battery_voltage();
+
+// Functions that take use of the 
+// Accelerometer and/or the Gyroscope
+void init_gyro_accel();
+void read_gyroscope();
+void read_accelerometer();
+
+// Functions that take use of the Encoders
+
+#endif // SENSORS_H
---
+++ lib/sensors.cpp
@@ -0,0 +1,160 @@
+#include "sensors.h"
+
+////////////////////////////////////////////////////////////
+// Proximity Sensors
+////////////////////////////////////////////////////////////
+
+/*
+ * FRONT_DIODES, SIDE_DIODES and DIAGONAL_DIODES 
+ * control mosfets that provide power to the emiters of the proximity
+ * sensors.  Before the value of the sensor is measured, a digital HIGH should
+ * be provided to the coresponding emiter.
+ * 
+ * WARNING: NEVER LIGHT UP ALL OF THE EMITERS SIMULTANIEUSLY, BECAUSE THE
+ * VOLTAGE REGULATOR MAY FAIL TO SUPPLY THE REQUIRED CURRENT AND THE
+ * MICROCONTROLLER MAY REBOOT
+ * 
+ * WARNING: NEVER POWER THE EMITERS FOR MORE THAN A COUPLE OF MICROSECONDS,
+ * BACAUSE DAMAGE OF THE SENSORS MAY OCCUR
+ * 
+ * ---------------------------------------------------------------------------
+ * 
+ * When we are reading the sensor values,
+ * the surounding light could affect the
+ * values read from the phototransistor.
+ * In order to eliminate that effect 
+ * as mutch as possible, we are making 
+ * two reading. One before lighting the emitter
+ * and one after that. Then we substract the dark value
+ * from the light value.
+ * 
+ * sensor_value = sensor_emitter_on - sensor_emitter_off;
+ * 
+ */
+
+void init_adc()
+{
+    adc->adc0->setAveraging(0);
+    adc->adc0->setResolution(10);
+    adc->adc0->setSamplingSpeed(ADC_SAMPLING_SPEED::MED_SPEED);
+    adc->adc0->setConversionSpeed(ADC_CONVERSION_SPEED::HIGH_SPEED);
+
+    adc->adc1->setAveraging(0);
+    adc->adc1->setResolution(10);
+    adc->adc1->setSamplingSpeed(ADC_SAMPLING_SPEED::MED_SPEED);
+    adc->adc1->setConversionSpeed(ADC_CONVERSION_SPEED::HIGH_SPEED);
+}
+
+void read_proximity_sensors()
+{
+    read_proximity_sensors_front();
+    read_proximity_sensors_diagonal();
+    read_proximity_sensors_side();
+}
+
+void read_proximity_sensors_front()
+{
+    emitter_off = adc->analogSyncRead(SENSOR_FRONT_LEFT, SENSOR_FRONT_RIGHT);
+
+    digitalWriteFast(FRONT_DIODES, HIGH);
+    delayMicroseconds(SENSOR_EMITTER_ON_TIME);
+
+    emitter_on = adc->analogSyncRead(SENSOR_FRONT_LEFT, SENSOR_FRONT_RIGHT);
+
+    digitalWriteFast(FRONT_DIODES, LOW);
+
+    front_left_sensor = (uint16_t)emitter_on->result_adc0 - (uint16_t)emitter_off->result_adc0;
+    front_right_sensor = (uint16_t)emitter_on->result_adc1 - (uint16_t)emitter_off->result_adc1;
+}
+
+void read_proximity_sensors_diagonal()
+{
+    emitter_off = adc->analogSyncRead(SENSOR_DIAGONALS_LEFT, SENSOR_DIAGONALS_RIGHT);
+
+    digitalWriteFast(DIAGONAL_DIODES, HIGH);
+    delayMicroseconds(SENSOR_EMITTER_ON_TIME);
+
+    emitter_on = adc->analogSyncRead(SENSOR_DIAGONALS_LEFT, SENSOR_DIAGONALS_RIGHT);
+    
+    digitalWriteFast(DIAGONAL_DIODES, LOW);
+
+    diagonal_left_sensor = (uint16_t)emitter_on->result_adc0 - (uint16_t)emitter_off->result_adc0;
+    diagonal_right_sensor = (uint16_t)emitter_on->result_adc1 - (uint16_t)emitter_off->result_adc1;
+}
+
+void read_proximity_sensors_side()
+{
+    emitter_off = adc->analogSyncRead(SENSOR_SIDES_LEFT, SENSOR_SIDES_RIGHT);
+
+    digitalWriteFast(SIDE_DIODES, HIGH);
+    delayMicroseconds(SENSOR_EMITTER_ON_TIME);
+
+    emitter_on = adc->analogSyncRead(SENSOR_SIDES_LEFT, SENSOR_SIDES_RIGHT);
+    
+    digitalWriteFast(SIDE_DIODES, LOW);
+
+    side_left_sensor = (uint16_t)emitter_on->result_adc0 - (uint16_t)emitter_off->result_adc0;
+    side_right_sensor = (uint16_t)emitter_on->result_adc1 - (uint16_t)emitter_off->result_adc1;
+}
+
+////////////////////////////////////////////////////////////
+// Battery Voltage
+////////////////////////////////////////////////////////////
+
+/* A voltage divider is attached to this pin, that measures the battery voltage.
+ * R1 = 18k 1%, R2 = 13k 1%
+ * By the voltage divider formula, when the input voltage is 7.869V, 
+ * the voltage provided to the BATTERY_VOLTAGE_LEVEL pin will be 3.3V.
+ */
+float read_battery_voltage()
+{
+    // Is it integer?   TODO: Check!
+    int adc_value = adc->adc0->analogRead(BATTERY_VOLTAGE_LEVEL);
+    float pin_voltage = ( (float)adc_value * 3.3 ) / adc->adc0->getMaxValue();
+
+    return (pin_voltage * 31) / 13;
+}
+
+////////////////////////////////////////////////////////////
+// Buttons
+////////////////////////////////////////////////////////////
+
+// TODO: Button functions
+
+////////////////////////////////////////////////////////////
+// Accelerometer and Gyroscope
+////////////////////////////////////////////////////////////
+
+// We have two approches to the Gyroscope, because
+// they are noisy little creacures!
+// First one, if the running speed does affect accuracy,
+// then we will set it to 1.66KHz and we will be fine.
+// The second option is to use higher sampling speed (6.66KHz)
+// and in the range of one ms to make 4 averages of the signal.
+
+void init_gyro_accel()
+{
+    lsm6ds33.begin_I2C();
+
+    // LSM6DS_ACCEL_RANGE_4_G
+    lsm6ds33.setAccelRange(LSM6DS_ACCEL_RANGE_2_G);
+
+    // LSM6DS_GYRO_RANGE_125_DPS
+    lsm6ds33.setGyroRange(LSM6DS_GYRO_RANGE_250_DPS);
+
+    // LSM6DS_RATE_833_HZ
+    lsm6ds33.setAccelDataRate(LSM6DS_RATE_1_66K_HZ);
+
+    // LSM6DS_RATE_833_HZ
+    lsm6ds33.setGyroDataRate(LSM6DS_RATE_1_66K_HZ);
+}
+
+void read_gyroscope()
+{
+    lsm6ds33.readGyroscope(gyro_x, gyro_y, gyro_z);
+}
+
+void read_accelerometer()
+{
+    lsm6ds33.readAcceleration(acc_x, acc_y, acc_z);
+}
